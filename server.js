a6d9fccb068b092dd6d00d07a9ed25dbb25eeb0b
binathperera <binathpr@gmail.com>
2023-03-13 22:38:19 +0530

    Add certificate file

--- server.js
+++ server.js
@@ -35,13 +35,24 @@ var options = {
 		}
 	}
 };
-var sessionStore = new MySQLStore(options);
 var connection = mysql.createConnection({
     host     : dbhost,
     user     : dbuser,
     password : dbpassword,
-    database : databaseName
+    database : databaseName,
+    ssl  : {
+        ca : fs.readFileSync(__dirname + '/certs/ca-certificate.crt')
+    },
   });
+// var del = connection._protocol._delegateError;
+// connection._protocol._delegateError = function(err, sequence){
+//   if (err.fatal) {
+//     console.trace('fatal error: ' + err.message);
+//   }
+//   return del.call(this, err, sequence);
+// };
+
+var sessionStore = new MySQLStore(options,connection);
 connection.connect();
 
 const app=express();
